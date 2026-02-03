812237dd378d791204e2503ddabdd29da1f05158
Gustavo Eloi <gustavooeloi@gmail.com>
2026-02-03 16:39:46 -0300

    refactor refund detail

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from "react-router";
+
+import { Loading } from "../components/Loading";
+
 import { AuthRoutes } from "./auth-routes";
 import { ManagerRoutes } from "./manager-routes";
 import { EmployeeRoutes } from "./employee-routes";
 
+const isLoading = true;
+
 export function Routes() {
   return (
-    <BrowserRouter>
-      <ManagerRoutes />
-    </BrowserRouter>
+    <BrowserRouter>{isLoading ? <Loading /> : <AuthRoutes />}</BrowserRouter>
   );
 }
--- src/routes/manager-routes.tsx
+++ src/routes/manager-routes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import { AppLayout } from "../components/AppLayout";
 import { Dashboard } from "../pages/Dashboard";
 import { RefundDetails } from "../pages/RefundDetails";
+import { Refund } from "../pages/Refund";
 
 export function ManagerRoutes() {
   return (
@@ -9,6 +10,7 @@ export function ManagerRoutes() {
       <Route path="/" element={<AppLayout />}>
         <Route path="/manager" element={<Dashboard />} />
         <Route path="/refund-details" element={<RefundDetails />} />
+        <Route path="/refund/:id" element={<Refund />} />
       </Route>
     </Routes>
   );
